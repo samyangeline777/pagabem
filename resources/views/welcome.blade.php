4b383bab74edec6f02550b978da91e53215989e2
Samilly Angeline <prof.samilly@outlook.com>
2017-06-25 15:55:07 -0300

    autenticação bancos dados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pagabem
                 </div>
 
                 <div class="links">
